76255c3db909729465188a3fa03716fa3e20e2a0
Vítor Teixeira <vitor_filipe_@hotmail.com>
2016-03-16 22:38:34 +0000

    some stuff

---
+++ TDIN1/TDIN1/Order.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDIN1
+{
+    public class Order
+    {
+
+        private uint id;
+        public uint Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        private String state;
+        public String State
+        {
+            get
+            {
+                return this.state;
+            }
+            set
+            {
+                this.state = value;
+            }
+        }
+
+        private String description;
+        public String Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+            }
+        }
+
+        private uint quantity;
+        public uint Quantity
+        {
+            get
+            {
+                return this.quantity;
+            }
+            set
+            {
+                this.quantity = value;
+            }
+        }
+
+        private uint tableId;
+        public uint TableId
+        {
+            get
+            {
+                return this.tableId;
+            }
+            set
+            {
+                this.tableId = value;
+            }
+        }
+
+        private bool type;
+        public bool Type
+        {
+            get
+            {
+                return this.type;
+            }
+            set
+            {
+                this.type = value;
+            }
+        }
+
+        private uint price;
+        public uint Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                this.price = value;
+            }
+        }
+    }
+}
---
+++ TDIN1/TDIN1/Register.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDIN1
+{
+    class Register
+    {
+        List<Order> ordersList;
+
+        //evento receber order
+    }
+}
